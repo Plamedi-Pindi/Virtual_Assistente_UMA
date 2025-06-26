c10ec3d9068c2f2901ca8c240b02017526918129
Plamedi-Pindi <plamedipindi77@gmial.com>
2025-06-26 15:58:10 +0100

    Fix: Modal Motion animation fixed

--- Frontend/src/components/Modal/Modal.tsx
+++ Frontend/src/components/Modal/Modal.tsx
@@ -11,7 +11,7 @@ const Modal = ({ modalDivRef, modalButtonRef, children }: ModalType) => {
 
     return (
         <div ref={modalDivRef} className={` w-full  bg-black/50 absolute z-[10]  backdrop-blur-[1.5px] overflow-hidden bottom-0 `} >
-            <div className="bg-third w-full absolute bottom-0 rounded-t-[1.5rem] p-4  pt-6 div-Child-Ref overflow-auto">
+            <div className="bg-third w-full h-[33rem] absolute bottom-0 rounded-t-[1.5rem] p-4  pt-6 div-Child-Ref ">
 
                 <button
                     ref={modalButtonRef}
