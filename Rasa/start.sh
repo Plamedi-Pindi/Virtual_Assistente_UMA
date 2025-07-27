d3b4ae41e9f144413949023f594bbdd57811f90b
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-28 00:37:09 +0100

    fix: update start.sh file

--- Rasa/start.sh
+++ Rasa/start.sh
@@ -6,5 +6,5 @@
 # Inicia o servidor de ações personalizado em segundo plano
 rasa run actions --port 5055 &
 
-# Inicia o servidor principal do Rasa
-rasa run --enable-api --cors "*" --debug --port 8000
+# Inicia o servidor principal do Rasa usando a porta exigida pela Render
+rasa run --enable-api --cors "*" --debug --port $PORT
