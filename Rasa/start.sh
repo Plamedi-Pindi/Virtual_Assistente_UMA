78939ed1691276f98f5d54c3deea86425a866d63
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-28 16:24:13 +0100

    feat: update start.sh

--- Rasa/start.sh
+++ Rasa/start.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Iniciando Rasa com porta $PORT"
 
+# Adiciona caminho onde rasa está instalado à variável PATH
+export PATH="/app/.local/bin:$PATH"
+
+# Executa o rasa com o PATH atualizado
 rasa run --enable-api --cors "*" --port ${PORT}
