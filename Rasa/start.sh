30844b3beb2c92df6cd4a93fe32e5c3b104da71a
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-28 16:14:10 +0100

    feat: update dockerfiles

--- Rasa/start.sh
+++ Rasa/start.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Ativa o ambiente virtual
-. ./venv/bin/activate
+echo "Iniciando Rasa com porta $PORT"
 
-# Inicia o servidor de ações personalizado em segundo plano
-rasa run actions --port 5055 &
-
-# Inicia o servidor principal do Rasa com canal REST ativado e usando a porta exigida pela Render
-rasa run \
-  --enable-api \
-  --cors "*" \
-  --debug \
-  --port $PORT \
-  --credentials credentials.render.yml
+rasa run --enable-api --cors "*" --port ${PORT}
