0b39b67ecd647f921fef5530995600b7a0143d7b
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-28 01:41:10 +0100

    feat: deploy update

--- Rasa/start.sh
+++ Rasa/start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Ativa o venv
+# Ativa o ambiente virtual
 . ./venv/bin/activate
 
 # Inicia o servidor de ações personalizado em segundo plano
 rasa run actions --port 5055 &
 
-# Inicia o servidor principal do Rasa usando a porta exigida pela Render
-rasa run --enable-api --cors "*" --debug --port $PORT
+# Inicia o servidor principal do Rasa com canal REST ativado e usando a porta exigida pela Render
+rasa run \
+  --enable-api \
+  --cors "*" \
+  --debug \
+  --port $PORT \
+  --credentials credentials.yml
