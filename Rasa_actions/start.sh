d99a665a695ffce6dbc73b746c82aaeb96306c73
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-29 22:35:17 +0100

    fix: update dockerfile

--- Rasa_actions/start.sh
+++ Rasa_actions/start.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
-echo "Iniciando action server na porta $PORT"
-
-exec python -m rasa_sdk.endpoint --port ${PORT:-5055}
+#!/bin/sh
+echo "PORT = $PORT"
+exec python -m rasa_sdk.endpoint --port $PORT
