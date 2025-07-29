4f7c1bb5e91a160094ada06aa58ce5ba263c6535
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-29 01:00:21 +0100

    fix: start.sh rasa command not found on rendernder deploy

--- Rasa/start.sh
+++ Rasa/start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Adiciona o local de instalação do Rasa ao PATH (necessário no container)
-export PATH="/app/.local/bin:$PATH"
-export PATH="/root/.local/bin:$PATH"
+echo "Verificando onde está o Rasa..."
+which rasa || echo "Rasa não está no PATH"
 
-echo "Iniciando custom actions na porta 5055..."
-rasa run actions --port 5055 &
+echo "Iniciando servidor de custom actions na porta 5055..."
+/usr/local/bin/rasa run actions --port 5055 &
 
-echo "Iniciando Rasa server na porta $PORT..."
-rasa run --enable-api --cors "*" --port $PORT
+echo "Iniciando servidor Rasa na porta $PORT..."
+/usr/local/bin/rasa run --enable-api --cors "*" --port $PORT
