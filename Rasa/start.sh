09b23c6b55bc4fdc3130fddad20e207afe25eb26
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-29 20:31:26 +0100

    fix: port in railway

--- Rasa/start.sh
+++ Rasa/start.sh
@@ -7,4 +7,5 @@ echo "Iniciando Rasa na porta $PORT"
 
 export PATH="/app/.local/bin:$PATH"
 
-rasa run --enable-api --cors "*" --port ${PORT:-5005}
+# rasa run --enable-api --cors "*" --port ${PORT:-5005 }
+rasa run --enable-api --cors "*" --port ${PORT}
