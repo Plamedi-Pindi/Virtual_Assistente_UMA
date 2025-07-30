583434fd4b4f6dc792d6e9ce740655e5a0bc5967
Plamedi-Pindi <plamedipindi77@gmail.com>
2025-07-30 12:56:14 +0100

    feat: use motion animation to onboarding image

--- Frontend/src/components/OnBoarding/OnBoarding.tsx
+++ Frontend/src/components/OnBoarding/OnBoarding.tsx
@@ -12,7 +12,9 @@ import img_boarding1 from "../../assets/img/onBoarding/1.png";
 import img_boarding2 from "../../assets/img/onBoarding/444.png";
 import img_boarding3 from "../../assets/img/onBoarding/333.png";
 
-import LoadingFallback from "../Load/Load";
+
+// Import Motion for animatio
+import { motion, AnimatePresence } from "motion/react";
 
 const onBoardingData = [
     {
@@ -34,7 +36,7 @@ const OnBoarding = () => {
     const [step, setStep] = useState(0);
     const navigate = useNavigate();
     const current = onBoardingData[step];
-  
+
     const nextStep = () => {
         if (step < onBoardingData.length - 1) {
             setStep(prev => prev + 1)
@@ -43,20 +45,33 @@ const OnBoarding = () => {
         }
     }
 
+    const imageData = [
+        { src: img_boarding1, top: '-11rem' },
+        { src: img_boarding2, top: '' }, // ajuste conforme necessário
+        { src: img_boarding3, top: '-1rem' },
+    ];
+
+    const currentImg = imageData[step] || imageData[0];
+
     return (
         <div className={`h-dvh 
             onBoard-stl object-contain  relative overflow-hidden  `}>
 
-            {step === 0
-                ? (
-                    <img src={img_boarding1} alt="" className="absolute -top-[11rem]  " />
-                )
-                : step === 1
-                    ? (<img src={img_boarding2} alt="" className="absolute -top-[rem]" />)
-                    : (<img src={img_boarding3} alt="" className="absolute -top-[1rem]" />)
-            }
+            <AnimatePresence mode="wait">
+                <motion.img
+                    key={currentImg.src} // importante para ativar a animação de saída/entrada
+                    src={currentImg.src}
+                    alt=""
+                    initial={{ opacity: 0, y: 30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -30 }}
+                    transition={{ duration: 0.5, ease: 'easeInOut' }}
+                    className="absolute w-full"
+                    style={{ top: currentImg.top }}
+                />
+            </AnimatePresence>
 
-            <div className="bg-[#e8e7e7] h-[18rem] w-full absolute -bottom-3 rounded-t-[2rem] p-2 mb-3 text-center">
+            <div className="bg-[#e8e7e7] h-[18rem] w-full absolute -bottom-3 rounded-t-[2rem] p-2 mb-4 text-center">
                 <div className="flex justify-center items-center mt-4 mb-5 gap-1 ">
                     <button className={` ${step === 0 ? 'w-7 bg-[#D72229]' : 'w-2 bg-[#D9D9D9]'}  h-2 rounded-full bloch`}></button>
                     <button className={`${step === 1 ? 'w-7 bg-[#D72229]' : 'w-2 bg-[#D9D9D9]'}  h-2 rounded-full  bloch`}></button>
@@ -72,7 +87,6 @@ const OnBoarding = () => {
                 <Button onClick={nextStep} >{step === 2 ? "começar agora" : "Próximo"}</Button>
             </div>
 
-            <LoadingFallback />
         </div>
     )
 }
